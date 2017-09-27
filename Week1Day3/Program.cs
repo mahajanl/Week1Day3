05d1db75db3f2a6410294c38470c1d94aed4a3ab
mahajanl <lakshamima@gmail.com>
2017-09-27 13:27:12 -0400

    conditionals start

--- Week1Day3/Program.cs
+++ Week1Day3/Program.cs
@@ -24,20 +24,48 @@ namespace Week1Day3
             //    Console.WriteLine("That makes sense.");
             //}
 
-            Console.WriteLine("What day of the week is it?");
-            string userAnswer = Console.ReadLine();
+            //Console.WriteLine("What day of the week is it?");
+            //string userAnswer = Console.ReadLine();
 
-            if(userAnswer == "Monday")
+            //if(userAnswer == "Monday")
+            //{
+            //    Console.WriteLine("Well that's unfortunate.");
+            //}
+            //else if(userAnswer == "Friday")
+            //{
+            //    Console.WriteLine("Happy Friday!!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You'll get through this!");
+            //}
+
+            Console.WriteLine("How old are you?");
+            int userAge = int.Parse(Console.ReadLine());
+
+            if(userAge <= 1)
             {
-                Console.WriteLine("Well that's unfortunate.");
+                Console.WriteLine("You're such a wittle thing!");
+
             }
-            else if(userAnswer == "Friday")
+            else if(userAge > 1 && userAge < 4)
             {
-                Console.WriteLine("Happy Friday!!");
+                Console.WriteLine("Daaaw, toddlers!");
+            }
+            else if(userAge >= 4 && userAge <13)
+            {
+                Console.WriteLine("Childhood is awesomeness. Have fun and don't grow up too fast.");
+            }
+            else if(userAge >= 13 && userAge < 65)
+            {
+                Console.WriteLine("Life gets awkward.");
+            }
+            else if(userAge >= 65 && userAge < 100)
+            { Console.WriteLine("Have a great retirement.");
             }
             else
             {
-                Console.WriteLine("You'll get through this!");
+                Console.WriteLine("That is definitely not your age.");
             }
         }
     }
