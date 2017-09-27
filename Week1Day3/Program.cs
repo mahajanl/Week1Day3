ad583289eaee4b31e8d2a270aa9ce05ed29d2fbc
mahajanl <lakshamima@gmail.com>
2017-09-27 12:03:14 -0400

    conditionals

--- Week1Day3/Program.cs
+++ Week1Day3/Program.cs
@@ -10,6 +10,35 @@ namespace Week1Day3
     {
         static void Main(string[] args)
         {
+            //int firstNumber = 2;
+            //int secondNumber = 6;
+
+            //if(firstNumber > secondNumber)
+            //{
+            //    //if condition is true, then do something
+            //    Console.WriteLine("You have broken math. Good job.");
+            //}
+            //else
+            //{
+            //    //do something else
+            //    Console.WriteLine("That makes sense.");
+            //}
+
+            Console.WriteLine("What day of the week is it?");
+            string userAnswer = Console.ReadLine();
+
+            if(userAnswer == "Monday")
+            {
+                Console.WriteLine("Well that's unfortunate.");
+            }
+            else if(userAnswer == "Friday")
+            {
+                Console.WriteLine("Happy Friday!!");
+            }
+            else
+            {
+                Console.WriteLine("You'll get through this!");
+            }
         }
     }
 }
